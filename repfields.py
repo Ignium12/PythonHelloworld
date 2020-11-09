31998c4e37001625983ac616cdd5bcad582ab745
Ignium12 <kevin.ruehl12@googlemail.com>
2020-11-09 14:31:05 +0100

    Initial Commit

---
+++ repfields.py
@@ -0,0 +1,14 @@
+age = 24
+print("My age is {} years".format(age))
+
+print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6} and {7}"
+      .format(31, "Jan", "Mar", "May", "Jul", "Aug", "oct", "Dec"))
+
+print("Jan: {2}, Feb: {0}, March: {2}".format(28,0,31))
+
+print()
+
+print("""Jan: {2}
+Feb: {0} 
+March: {2}
+""".format(28,0,31))
---
+++ sliceback.py
@@ -0,0 +1,15 @@
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+backwards = letters[::-1] # reversing the sequence
+print(backwards)
+
+print(letters[16:13:-1]) # qpo
+
+print(letters[4::-1]) #edcba
+
+print(letters[:-9:-1])
+
+print(letters[-4:])  # gets the last items
+print(letters[-1:])
+print(letters[:1])
+print(letters[0])  # error when String is empty
